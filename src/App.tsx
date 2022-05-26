b974284f0431e7070576fd86ec55a5db0a1e27e3
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-26 14:43:59 +0300

    add some style to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import Header from "./Header";
-import Technologies from "./Technologies";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
 
 function App() {
     return (
-        <div>
+        <div className='app-wrapper'>
             <Header/>
-            <Technologies/>
+            <Navbar/>
+            <Profile/>
         </div>);
 }
 
