82d74ed37f05ca449442bc16ce428da4315e8632
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-26 14:43:30 +0300

    create new components

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img
+                src="https://www.freepnglogos.com/uploads/ukraine-flag-png/circle-flag-of-ukraine-png-download-0.png"
+                alt="logo"/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://cdn.pixabay.com/photo/2017/08/22/00/38/meadow-2667461_960_720.jpg" alt="bg"/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>New posts</div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
