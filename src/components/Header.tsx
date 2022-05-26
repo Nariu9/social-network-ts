3c536112884cbfd8ff1cfbfd74feb26678fe7263
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-05-26 16:37:11 +0300

    add css-modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <img
                 src="https://www.freepnglogos.com/uploads/ukraine-flag-png/circle-flag-of-ukraine-png-download-0.png"
                 alt="logo"/>
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
                 <img src="https://cdn.pixabay.com/photo/2017/08/22/00/38/meadow-2667461_960_720.jpg" alt="bg"/>
             </div>
@@ -13,9 +14,9 @@ const Profile = () => {
                 My posts
                 <div>New posts</div>
             </div>
-            <div>
-                <div>Post 1</div>
-                <div>Post 2</div>
+            <div className={classes.posts}>
+                <div className={classes.item}>Post 1</div>
+                <div className={classes.item}>Post 2</div>
             </div>
         </div>
     )
