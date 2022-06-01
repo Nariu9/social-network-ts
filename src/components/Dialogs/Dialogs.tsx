e96f3e4bb9f983acf796b7c331529d8edd705209
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-06-01 18:25:47 +0300

    add layout and style to Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,7 +3,21 @@ import classes from "./Dialogs.module.css";
 
 const Dialogs = () => {
     return (
-        <div>Dialogs</div>
+        <div className={classes.dialogs}>
+            <div className={classes.dialogItems}>
+                <div className={`${classes.dialog} ${classes.active}`}>Dimych</div>
+                <div className={classes.dialog}>Andrey</div>
+                <div className={classes.dialog}>Sveta</div>
+                <div className={classes.dialog}>Sasha</div>
+                <div className={classes.dialog}>Virtor</div>
+                <div className={classes.dialog}>Masha</div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>Hi</div>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>Let's go</div>
+            </div>
+        </div>
     )
 }
 
