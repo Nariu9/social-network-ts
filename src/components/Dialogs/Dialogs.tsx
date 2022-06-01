892234764c2bd1be73714b13a746c8ec8a155f11
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-06-01 18:46:03 +0300

    add <NavLink> and style to Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import classes from "./Dialogs.module.css";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = () => {
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
-                <div className={`${classes.dialog} ${classes.active}`}>Dimych</div>
-                <div className={classes.dialog}>Andrey</div>
-                <div className={classes.dialog}>Sveta</div>
-                <div className={classes.dialog}>Sasha</div>
-                <div className={classes.dialog}>Virtor</div>
-                <div className={classes.dialog}>Masha</div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/1'} activeClassName={classes.active}>Dimych</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/2'} activeClassName={classes.active}>Andrey</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/3'} activeClassName={classes.active}>Sveta</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/4'} activeClassName={classes.active}>Sasha</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/5'} activeClassName={classes.active}>Virtor</NavLink>
+                </div>
+                <div className={classes.dialog}>
+                    <NavLink to={'/dialogs/6'} activeClassName={classes.active}>Masha</NavLink>
+                </div>
             </div>
             <div className={classes.messages}>
                 <div className={classes.message}>Hi</div>
