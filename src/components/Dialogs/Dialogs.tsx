5546712f5de315e241babab27cb1a8a6b9c07281
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-06-05 22:06:01 +0300

    make BLL in Dialogs and MyPosts components

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -25,22 +25,35 @@ return <div className={classes.message}>{props.message}</div>
 }
 
 const Dialogs = () => {
+
+    let dialogsData = [
+        {id:1, name:'Dimych'},
+        {id:2, name:'Andrey'},
+        {id:3, name:'Sveta'},
+        {id:4, name:'Sasha'},
+        {id:5, name:'Viktor'},
+        {id:6, name:'Masha'}
+    ]
+
+    let messagesData = [
+        {id:1, message:'Hi'},
+        {id:2, message:'Ho'},
+        {id:3, message:'Let\'s go'},
+        {id:4, message:'Hi'},
+        {id:5, message:'Hi'},
+        {id:6, message:'Hi'}
+    ]
+
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
-                <DialogItem name={'Dimych'} id={1}/>
-                <DialogItem name={'Andrey'} id={2}/>
-                <DialogItem name={'Sveta'} id={3}/>
-                <DialogItem name={'Sasha'} id={5}/>
-                <DialogItem name={'Viktor'} id={6}/>
-                <DialogItem name={'Masha'} id={7}/>
+                <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
             </div>
             <div className={classes.messages}>
-                <Message message={'Hi'}/>
-                <Message message={'Let\'s go'}/>
-                <Message message={'Hi'}/>
-                <Message message={'Hi'}/>
-                <Message message={'Hi'}/>
+                <Message message={messagesData[0].message}/>
+                <Message message={messagesData[1].message}/>
+
             </div>
         </div>
     )
