3bde53fef2dcf56a765fade5018344021073c5ea
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-06-09 17:30:43 +0300

    move BLL (App state) from index.tsx to state.ts and refactor it

---
+++ src/redux/state.ts
@@ -0,0 +1,31 @@
+let state = {
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, how are you?', likesCount: 15},
+            {id: 2, message: 'It\'s my first post', likesCount: 20},
+            {id: 3, message: 'Or it is not?', likesCount: 3},
+            {id: 4, message: 'Hah...', likesCount: 0},
+        ]
+    },
+    dialogsPage: {
+        dialogs: [
+            {id: 1, name: 'Dimych'},
+            {id: 2, name: 'Andrey'},
+            {id: 3, name: 'Sveta'},
+            {id: 4, name: 'Sasha'},
+            {id: 5, name: 'Viktor'},
+            {id: 6, name: 'Masha'}
+        ],
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Ho'},
+            {id: 3, message: 'Let\'s go'},
+            {id: 4, message: 'Hi'},
+            {id: 5, message: 'Hi'},
+            {id: 6, message: 'Hi'}
+        ]
+    }
+    /*sidebar:{}*/
+}
+
+export default state
