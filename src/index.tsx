b8f665d5326639db6fed76e6be6f43b2c495cc8f
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-06-09 17:39:42 +0300

    move BrowserRouter from App to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,34 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
-let posts = [
-    {id: 1, message: 'Hi, how are you?', likesCount: 15},
-    {id: 2, message: 'It\'s my first post', likesCount: 20},
-    {id: 3, message: 'Or it is not?', likesCount: 3},
-    {id: 4, message: 'Hah...', likesCount: 0},
-]
-
-let dialogs = [
-    {id: 1, name: 'Dimych'},
-    {id: 2, name: 'Andrey'},
-    {id: 3, name: 'Sveta'},
-    {id: 4, name: 'Sasha'},
-    {id: 5, name: 'Viktor'},
-    {id: 6, name: 'Masha'}
-]
-
-let messages = [
-    {id: 1, message: 'Hi'},
-    {id: 2, message: 'Ho'},
-    {id: 3, message: 'Let\'s go'},
-    {id: 4, message: 'Hi'},
-    {id: 5, message: 'Hi'},
-    {id: 6, message: 'Hi'}
-]
-
+import state from "./redux/state";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.render(
-    <App posts={posts} dialogs={dialogs} messages={messages}/>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+        <App state={state}/>
+    </BrowserRouter>, document.getElementById('root'));
