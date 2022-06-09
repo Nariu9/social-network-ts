ec94e112d1940da26047aa60d9a68472b4a68347
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-06-09 17:35:29 +0300

    add typing to components props

--- src/App.tsx
+++ src/App.tsx
@@ -3,49 +3,60 @@ import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
+import {Route} from "react-router-dom";
 import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 
 type AppPropsType = {
-    posts:Array<PostPropsType>
-    dialogs:Array<DialogPropsType>
-    messages:Array<MessagePropsType>
+    state: StateType
+}
+
+type StateType = {
+    profilePage: ProfilePagePropsType
+    dialogsPage: DialogsPagePropsType
+}
+
+export type ProfilePagePropsType = {
+    posts: Array<PostPropsType>
+}
+
+export type DialogsPagePropsType = {
+    dialogs: Array<DialogPropsType>
+    messages: Array<MessagePropsType>
 }
 
 export type PostPropsType = {
-    id:number
-    message:string
-    likesCount:number
+    id: number
+    message: string
+    likesCount: number
 }
 
 export type DialogPropsType = {
-    id:number
-    name:string
+    id: number
+    name: string
 }
 
 export type MessagePropsType = {
-    id:number
-    message:string
+    id: number
+    message: string
 }
 
 function App(props: AppPropsType) {
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Route path={'/dialogs'} render={()=><Dialogs dialogs={props.dialogs} messages={props.messages}/>}/>
-                    <Route path={'/profile'} render={()=><Profile posts={props.posts}/>}/>
-                    <Route path={'/news'} render={()=><News/>}/>
-                    <Route path={'/music'} render={()=><Music/>}/>
-                    <Route path={'/settings'} render={()=><Settings/>}/>
-                </div>
+        <div className='app-wrapper'>
+            <Header/>
+            <Navbar/>
+            <div className='app-wrapper-content'>
+                <Route path={'/dialogs'} render={() => <Dialogs state={props.state.dialogsPage}/>}/>
+                <Route path={'/profile'} render={() => <Profile state={props.state.profilePage}/>}/>
+                <Route path={'/news'} render={() => <News/>}/>
+                <Route path={'/music'} render={() => <Music/>}/>
+                <Route path={'/settings'} render={() => <Settings/>}/>
             </div>
-        </BrowserRouter>);
+        </div>
+    );
 }
 
 export default App;
