185053c5e6d3b22118839a8bade735de4f3661d6
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-13 22:39:25 +0300

    rename reducers to rootReducer

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -12,17 +12,17 @@ import {ActionType} from "./store";
 
 export type ReduxStoreType = Store<ReduxStateType, ActionType>
 export type ReduxStateType = ReturnType<RootState> // ReturnType что функция возвращает
-export type RootState = typeof reducers;  // типизация функции combineReducers
+export type RootState = typeof rootReducer;  // типизация функции combineReducers
 
 // export type ReduxStateType = ReturnType<typeof reducers> короткая запись строк 14-15
 
 
-const reducers = combineReducers({
+const rootReducer = combineReducers({   //один большой редьюсер
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer
 })
 
-const store: ReduxStoreType = createStore(reducers)     //возможно типизация store излишня
+const store: ReduxStoreType = createStore(rootReducer)     //возможно типизация store излишня
 
 export default store
