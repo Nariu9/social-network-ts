ae0b9ee3b0a5a2ae60ad3214bb09701ee0b6090f
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-14 17:30:48 +0300

    delete renderEntireTree function

--- src/index.tsx
+++ src/index.tsx
@@ -6,14 +6,9 @@ import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-
-const renderEntireTree = () => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}><App/></Provider>
-        </BrowserRouter>, document.getElementById('root'));
-}
-
-renderEntireTree()
-
-store.subscribe(renderEntireTree)
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </BrowserRouter>, document.getElementById('root'));
