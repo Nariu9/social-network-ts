ab34f308f0fb46f190d832ea478d0deb79166255
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-15 15:42:16 +0300

    remove useless type and import

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import classes from './Profile.module.css';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
-import {ReduxStoreType} from "../../redux/redux-store";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
 
 
-
-
-/*type PostsPropsType = {
-    store: ReduxStoreType
-}*/
-
 const Profile = () => {
-
     return (
         <div>
             <ProfileInfo/>
