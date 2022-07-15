57fcf72c52e53ff365fe0cbcb71accc3d624e003
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-15 15:46:51 +0300

    create user-reducer.ts

---
+++ src/redux/users-reducer.ts
@@ -0,0 +1,42 @@
+export type LocationType = {
+    city: string
+    country: string
+}
+export type UserType = {
+    id: number
+    photoUrl: string
+    followed: boolean
+    fullName: string
+    status: string
+    location: LocationType
+}
+export type UsersPageStateType = {
+    users: UserType[]
+}
+
+const FOLLOW = 'FOLLOW';
+const UNFOLLOW = 'UNFOLLOW';
+const SET_USERS = 'SET_USERS';
+
+const initialState: UsersPageStateType = {
+    users: []
+}
+
+export const usersReducer = (state: UsersPageStateType = initialState, action: UsersActionsType): UsersPageStateType => {
+    switch (action.type) {
+        case FOLLOW:
+            return {...state, users: state.users.map(u => u.id === action.userID ? {...u, followed: true} : u)}
+        case UNFOLLOW:
+            return {...state, users: state.users.map(u => u.id === action.userID ? {...u, followed: false} : u)}
+        case SET_USERS:
+            return {...state, users: [...state.users, ...action.users]}
+        default:
+            return state;
+    }
+}
+
+export type UsersActionsType = ReturnType<typeof followAC> | ReturnType<typeof unfollowAC> | ReturnType<typeof setUsersAC>
+
+export const followAC = (userID: number) => ({type: FOLLOW, userID}) as const
+export const unfollowAC = (userID: number) => ({type: UNFOLLOW, userID}) as const
+export const setUsersAC = (users: UserType[]) => ({type: SET_USERS, users}) as const
