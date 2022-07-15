88d143ec4b45bde598dc84c7800f51ee516f54e3
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-15 15:40:52 +0300

    refactor ReduxStateType and add some comments

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,8 +1,8 @@
-import {combineReducers, createStore, Store} from "redux";
+import {combineReducers, createStore} from "redux";
 import {profileReducer} from "./profile-reducer";
 import {dialogsReducer} from "./dialogs-reducer";
 import {sidebarReducer} from "./sidebar-reducer";
-import {ActionType} from "./store";
+import {usersReducer} from "./users-reducer";
 
 /*export type ReduxStoreType = Store<EmptyObject & { profilePage: ProfilePagePropsType; dialogsPage: DialogsPagePropsType; sidebar: {}; }, any>*/
 //типизация из подсказки TS при передаче store в App в файле index.tsx
@@ -10,20 +10,21 @@ import {ActionType} from "./store";
 //export type ReduxStoreType = Store<StateType, ActionType>
 //типизация с использованием типа StateType от старого store из файла store.ts
 
-export type ReduxStoreType = Store<ReduxStateType, ActionType>
-export type ReduxStateType = ReturnType<RootState> // ReturnType что функция возвращает
-export type RootState = typeof rootReducer;  // типизация функции combineReducers
+//export type ActionType = ProfileActionType | DialogsActionsType | UsersActionsType    //типизация экшенов, возможно излишня
+//export type ReduxStoreType = Store<ReduxStateType, ActionType>                        //типизация ReduxStore, возможно излишня
+//export type ReduxStateType = ReturnType<RootState> // ReturnType что функция возвращает
+//export type RootState = typeof rootReducer;  // типизация функции combineReducers
 
-// export type ReduxStateType = ReturnType<typeof reducers> короткая запись строк 14-15
-
+export type ReduxStateType = ReturnType<typeof rootReducer>  //короткая запись строк 15-16
 
 const rootReducer = combineReducers({   //один большой редьюсер
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
+    usersPage: usersReducer,
     sidebar: sidebarReducer
 })
 
-const store: ReduxStoreType = createStore(rootReducer)     //возможно типизация store излишня
+const store = createStore(rootReducer)     //возможно типизация store излишня
 
 //@ts-ignore
 window.store=store
