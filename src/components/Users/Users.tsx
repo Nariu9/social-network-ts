2deac91e49fad7535a326ca26faceb8ed01facee
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-30 08:40:35 +0300

    refactor Users back to functional component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,69 +1,33 @@
+import {UserType} from '../../redux/users-reducer';
 import React from 'react';
-import {UsersPropsType} from './UsersContainer';
 import classes from './Users.module.css';
-import axios from 'axios';
-import userPhoto from '../../assets/images/userPhoto.jpg'
+import userPhoto from '../../assets/images/userPhoto.jpg';
 
-/*props.setUsers([
-           {
-               id: 1,
-               photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHFvLzZ3fP-H6wwarV4QtowIqDEujp-IcNXA&usqp=CAU',
-               followed: false,
-               fullName: 'Dmitry',
-               status: 'I\'m boss',
-               location: {city: 'Tokyo', country: 'Japan'}
-           },
-           {
-               id: 2,
-               photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHFvLzZ3fP-H6wwarV4QtowIqDEujp-IcNXA&usqp=CAU',
-               followed: true,
-               fullName: 'Alex',
-               status: 'I\'m driving',
-               location: {city: 'Hel', country: 'Poland'}
-           },
-           {
-               id: 3,
-               photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHFvLzZ3fP-H6wwarV4QtowIqDEujp-IcNXA&usqp=CAU',
-               followed: false,
-               fullName: 'Oleg',
-               status: 'where am I ?',
-               location: {city: 'Rome', country: 'Italy'}
-           }
-       ])*/
+type UsersPropsType = {
+    totalUsersCount: number
+    pageSize: number
+    currentPage: number
+    users: UserType[]
+    onPageChanged: (page: number) => void
+    follow: (id: number) => void
+    unfollow: (id: number) => void
+}
+export const Users: React.FC<UsersPropsType> = (props) => {
 
-class Users extends React.Component<UsersPropsType> {
-
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
-        });
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+    let pages = []
+    for (let i = 1; i <= pagesCount; i++) {
+        if (pages.length < 10) pages.push(i)
     }
 
-    onPageChanged = (pageNumber: number) => {
-        this.props.setCurrentPage(pageNumber)
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
-            this.props.setUsers(response.data.items)
-        });
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
-        let pages = []
-        for (let i = 1; i <= pagesCount; i++) {
-            if (pages.length < 10) pages.push(i)
-        }
-
-        return (
-            <div>
-                <div className={classes.pages}>
-                    {pages.map(p => <span key={p}
-                                          onClick={() => this.onPageChanged(p)}
-                                          className={this.props.currentPage === p ? classes.selectedPage : ''}>{p}</span>)}
-                </div>
-                {
-                    this.props.users.map(u => <div key={u.id}>
+    return <div>
+        <div className={classes.pages}>
+            {pages.map(p => <span key={p}
+                                  onClick={() => props.onPageChanged(p)}
+                                  className={props.currentPage === p ? classes.selectedPage : ''}>{p}</span>)}
+        </div>
+        {
+            props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img
@@ -72,11 +36,11 @@ class Users extends React.Component<UsersPropsType> {
                         </div>
                         <div>
                             {u.followed
-                                ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button>
-                                : <button onClick={() => this.props.follow(u.id)}>Follow</button>}
+                                ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button>
+                                : <button onClick={() => props.follow(u.id)}>Follow</button>}
                         </div>
                     </span>
-                        <span>
+                <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -86,10 +50,6 @@ class Users extends React.Component<UsersPropsType> {
                             <div>{'u.location.city'}</div>
                         </span>
                     </span>
-                    </div>)
-                }</div>
-        );
-    }
+            </div>)
+        }</div>
 }
-
-export default Users;
