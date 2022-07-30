240554d29be52445ca464a04d0601f1569690f74
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-30 08:47:06 +0300

    refactor the UsersContainer class component to put it in the Connect container component

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import Users from './Users';
 import {
     followAC,
     setCurrentPageAC,
@@ -11,6 +10,9 @@ import {
 } from '../../redux/users-reducer';
 import {ReduxStateType} from '../../redux/redux-store';
 import {Dispatch} from 'redux';
+import axios from 'axios';
+import {Users} from './Users';
+
 
 type mapStateToPropsType = {
     users: UserType[]
@@ -25,7 +27,35 @@ type mapDispatchToProps = {
     setCurrentPage: (pageNumber: number) => void
     setTotalUsersCount: (usersCount: number) => void
 }
-export type UsersPropsType = mapStateToPropsType & mapDispatchToProps
+type UsersAPIContainerPropsType = mapStateToPropsType & mapDispatchToProps
+
+
+class UsersContainer extends React.Component<UsersAPIContainerPropsType> {
+
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        });
+    }
+
+    onPageChanged = (pageNumber: number) => {
+        this.props.setCurrentPage(pageNumber)
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`).then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      currentPage={this.props.currentPage}
+                      users={this.props.users}
+                      onPageChanged={this.onPageChanged}
+                      follow={this.props.follow}
+                      unfollow={this.props.unfollow}/>
+    }
+}
 
 const mapStateToProps = (state: ReduxStateType): mapStateToPropsType => {
     return {
@@ -46,6 +76,4 @@ const mapDispatchToProps = (dispatch: Dispatch): mapDispatchToProps => {
     }
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
