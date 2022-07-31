ef69557366e106e36b885d8519739cfd7cabef36
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-31 09:24:36 +0300

    replace Profile component with ProfileContainer component

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,11 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import {Redirect, Route} from "react-router-dom";
-import Profile from "./components/Profile/Profile";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import UsersContainer from "./components/Users/UsersContainer";
+import ProfileContainer from './components/Profile/ProfileContainer';
 
 
 function App() {
@@ -20,7 +20,7 @@ function App() {
             <div className='app-wrapper-content'>
                 <Route path={'/'} exact render={() => <Redirect to={'/profile'}/>}/>
                 <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
-                <Route path={'/profile'} render={() => <Profile/>}/>
+                <Route path={'/profile'} render={() => <ProfileContainer/>}/>
                 <Route path={'/users'} render={() => <UsersContainer/>}/>
                 <Route path={'/news'} render={() => <News/>}/>
                 <Route path={'/music'} render={() => <Music/>}/>
