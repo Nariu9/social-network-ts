7a13e8fba5606ec4a8519db894a5c268599864d6
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-31 16:51:17 +0300

    add optional parameters to ProfileContainer Route path

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
             <div className='app-wrapper-content'>
                 <Route path={'/'} exact render={() => <Redirect to={'/profile'}/>}/>
                 <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
-                <Route path={'/profile'} render={() => <ProfileContainer/>}/>
+                <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
                 <Route path={'/users'} render={() => <UsersContainer/>}/>
                 <Route path={'/news'} render={() => <News/>}/>
                 <Route path={'/music'} render={() => <Music/>}/>
