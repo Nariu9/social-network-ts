685c00ef455fe98e96095c6cf2a417847f574115
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-07-31 09:12:30 +0300

    add ts-ignore comment to dispatch in store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -94,6 +94,7 @@ let store: StoreType = {
     },
 
     dispatch(action) {
+        // @ts-ignore
         this._state.profilePage = profileReducer(this._state.profilePage, action)
         this._state.dialogsPage = dialogsReducer(this._state.dialogsPage, action)
         this._state.sidebar = sidebarReducer(this._state.sidebar, action)
