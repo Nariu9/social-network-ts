31866c61d143b1e3e0584f697493721c62403083
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-04 15:25:02 +0300

    add auth reducer to combineReducers and add Redux DevTools

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,30 +1,33 @@
-import {combineReducers, createStore} from 'redux';
+import {combineReducers, compose, createStore} from 'redux';
 import {profileReducer} from './profile-reducer';
 import {dialogsReducer} from './dialogs-reducer';
 import {sidebarReducer} from './sidebar-reducer';
 import {usersReducer} from './users-reducer';
-
-/*export type ReduxStoreType = Store<EmptyObject & { profilePage: ProfilePagePropsType; dialogsPage: DialogsPagePropsType; sidebar: {}; }, any>*/
-//типизация из подсказки TS при передаче store в App в файле index.tsx
-
-//export type ReduxStoreType = Store<StateType, ActionType>
-//типизация с использованием типа StateType от старого store из файла store.ts
+import {authReducer} from './auth-reducer';
 
 //export type ActionType = ProfileActionType | DialogsActionsType | UsersActionsType    //типизация экшенов, возможно излишня
 //export type ReduxStoreType = Store<ReduxStateType, ActionType>                        //типизация ReduxStore, возможно излишня
 //export type ReduxStateType = ReturnType<RootState> // ReturnType что функция возвращает
 //export type RootState = typeof rootReducer;  // типизация функции combineReducers
 
-export type ReduxStateType = ReturnType<typeof rootReducer>  //короткая запись строк 15-16
+export type ReduxStateType = ReturnType<typeof rootReducer>  //короткая запись строк 10-11
+
+declare global {
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
+}
 
 const rootReducer = combineReducers({   //один большой редьюсер
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
-    sidebar: sidebarReducer
+    sidebar: sidebarReducer,
+    auth: authReducer
 })
 
-const store = createStore(rootReducer)     //возможно типизация store излишня
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers())
 
 //@ts-ignore
 window.store = store
