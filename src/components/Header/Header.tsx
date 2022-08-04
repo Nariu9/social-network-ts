c3390b0ad1220906fa86e0d600fcbcf7ccfbe634
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-04 15:29:32 +0300

    pass props from HeaderContainer component to Header component to display login status

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React from 'react';
 import classes from './Header.module.css';
+import {NavLink} from 'react-router-dom';
 
-const Header = () => {
+type HeaderPropsType = {
+    isAuth: boolean
+    login: string | null
+}
+
+const Header = ({isAuth, login}: HeaderPropsType) => {
     return (
         <header className={classes.header}>
             <img
                 src="https://www.freepnglogos.com/uploads/ukraine-flag-png/circle-flag-of-ukraine-png-download-0.png"
                 alt="logo"/>
+            <div className={classes.loginBlock}>
+                {isAuth ? login : <NavLink to={'/login'}>Login</NavLink>}
+            </div>
         </header>
     )
 }
