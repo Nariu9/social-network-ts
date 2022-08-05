dece0e06a69ababf7d52565e0357b2bf544d69ef
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-05 13:21:10 +0300

    create DAL

---
+++ src/api/api.ts
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        'API-KEY': '219ce503-db19-4440-86cc-f93419479856'
+    }
+})
+
+export const usersAPI = {
+    getUsers (currentPage: number = 1, pageSize: number = 5) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`).then(response => response.data)
+    },
+    getAuth () {
+        return instance.get(`auth/me`).then(response => response.data)
+    },
+    followUser (userId: number) {
+        return instance.post(`follow/${userId}`).then(response => response.data)
+    },
+    unfollowUser (userId: number) {
+        return instance.delete(`follow/${userId}`).then(response => response.data)
+    }
+}
