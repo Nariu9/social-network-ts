6e68650ce6664a642b43df53ba4fddaecbcae5dc
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-12 09:25:42 +0300

    add route to Login component

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Settings from "./components/Settings/Settings";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
+import Login from './components/Login/Login';
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
                 <Route path={'/news'} render={() => <News/>}/>
                 <Route path={'/music'} render={() => <Music/>}/>
                 <Route path={'/settings'} render={() => <Settings/>}/>
+                <Route path={'/login'} render={() => <Login/>}/>
             </div>
         </div>
     );
