1f41745ff264b2e601b444e59bc0014be31e991e
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-08-16 07:38:56 +0300

    remove unused code from Dialogs component

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,6 @@ import classes from "./Dialogs.module.css";
 import {DialogItem} from "./DialogItem/DialogItem";
 import {Message} from "./Message/Message";
 import {DialogsPropsType} from "./DialogsContainer";
-import {Redirect} from 'react-router-dom';
-
 
 const Dialogs = (props: DialogsPropsType) => {
 
@@ -19,8 +17,6 @@ const Dialogs = (props: DialogsPropsType) => {
     const onMessageChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
         props.updateNewMessageText(event.currentTarget.value)
     }
-
-    if (!props.isAuth) return <Redirect to={'/login'}/>
 
     return (
         <div className={classes.dialogs}>
