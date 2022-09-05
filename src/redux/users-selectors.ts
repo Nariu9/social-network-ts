7199ba80bf63ca654e3d9a30fd26fb3b45e8951b
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-05 22:07:07 +0300

    create getUsers selector with fake filtering using Reselect library

--- src/redux/users-selectors.ts
+++ src/redux/users-selectors.ts
@@ -1,10 +1,13 @@
 import {RootState} from './redux-store';
+import {createSelector} from 'reselect';
+import {UserType} from './users-reducer';
 
-export const getUsers = (state: RootState) => state.usersPage.users
+const getUsersSelector = (state: RootState) => state.usersPage.users
+export const getUsers = createSelector(getUsersSelector, (users: UserType[]) => {
+    return users.filter(u => true)
+})
 export const getPageSize = (state: RootState) => state.usersPage.pageSize
 export const getTotalUsersCount = (state: RootState) => state.usersPage.totalUsersCount
 export const getCurrentPage = (state: RootState) => state.usersPage.currentPage
 export const getIsFetching = (state: RootState) => state.usersPage.isFetching
 export const getFollowingInProgress = (state: RootState) => state.usersPage.followingInProgress
-
-
