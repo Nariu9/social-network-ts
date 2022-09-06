e8257fbf91562755e66a7347bc6429aa823bb776
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-06 06:32:29 +0300

    create ProfileStatusWithHooks functional component using useState hook

---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.tsx
@@ -0,0 +1,34 @@
+import React, {ChangeEvent, useState} from 'react';
+
+type ProfileStatusPropsType = {
+    status: string,
+    updateStatus: (status: string) => void
+}
+
+export const ProfileStatusWithHooks: React.FC<ProfileStatusPropsType> = (props) => {
+    const [editMode, setEditMode] = useState(false)
+    const [status, setStatus] = useState(props.status)
+
+    const activateEditMode = () => setEditMode(true)
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+    const onStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
+            setStatus(e.currentTarget.value)
+    }
+
+    return <>
+        {!editMode && <div style={{fontWeight: 'bold'}}>
+                <span onDoubleClick={activateEditMode}>
+                    {status || '--------'}
+                </span>
+        </div>}
+        {editMode && <div>
+            <input value={status}
+                   onBlur={deactivateEditMode}
+                   autoFocus
+                   onChange={onStatusChange}/>
+        </div>}
+    </>
+}
