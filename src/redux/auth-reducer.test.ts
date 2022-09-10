7f280a36f5550c62cc893254a06159179c4ab68f
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-10 21:42:28 +0300

    create auth reducer test

---
+++ src/redux/auth-reducer.test.ts
@@ -0,0 +1,13 @@
+import {authReducer, AuthStateType, setAuthUserData} from './auth-reducer';
+
+test('user data should be set to state', () => {
+
+    const startState: AuthStateType = {id: null, login: null, email: null, isAuth: false}
+
+    const endState = authReducer(startState, setAuthUserData(1, 'John', 'email@email.com', true))
+
+    expect(endState.id).toBe(1)
+    expect(endState.login).toBe('John')
+    expect(endState.email).toBe('email@email.com')
+    expect(endState.isAuth).toBe(true)
+})
