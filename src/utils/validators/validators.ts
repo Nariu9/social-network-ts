65fa9295df504baaaf936fcee14ba1f975946187
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-14 09:39:46 +0300

    create validators types

--- src/utils/validators/validators.ts
+++ src/utils/validators/validators.ts
@@ -3,7 +3,10 @@ export const required = (value: string) => {
     return 'Field is required'
 }
 
-export const maxLengthCreator = (maxLength: number) => (value: string)=> {
+export const maxLengthCreator = (maxLength: number) => (value: string) => {
     if (value.length > maxLength) return `Max length is ${maxLength} symbols`
     return undefined
 }
+
+export type ValidatorType = typeof required | typeof maxLengthCreator
+export type ValidatorsType = typeof required & typeof maxLengthCreator
