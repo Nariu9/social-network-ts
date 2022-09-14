11eaa8609467be5ebc294b16bddef60b9d1f3c92
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-14 14:47:25 +0300

    add props destructuring

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -3,41 +3,38 @@ import classes from './ProfileInfo.module.css';
 import {ProfileType} from '../../../redux/profile-reducer';
 import userPhoto from '../../../assets/images/userPhoto.jpg'
 import {Preloader} from '../../common/Preloader/Preloader';
-import {ProfileStatus} from './ProfileStatus';
+// import {ProfileStatus} from './ProfileStatus';
 import {ProfileStatusWithHooks} from './ProfileStatusWithHooks';
 
 type ProfileInfoPropsType = {
     profile: null | ProfileType,
     status: string,
-    updateStatus:(status: string) => void
+    updateStatus: (status: string) => void
 }
 
-const ProfileInfo = (props: ProfileInfoPropsType) => {
-    if (!props.profile) {
+const ProfileInfo: React.FC<ProfileInfoPropsType> = ({profile, status, updateStatus}) => {
+    if (!profile) {
         return <Preloader/>
     }
 
     return (
         <div>
-            {/*<div>
-                <img src="https://cdn.pixabay.com/photo/2017/08/22/00/38/meadow-2667461_960_720.jpg" alt="bg"/>
-            </div>*/}
             <div className={classes.descriptionBlock}>
-                <img src={props.profile.photos.large ? props.profile.photos.large : userPhoto} alt="user"/>
-                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
-                <div>Hi, my name is {props.profile.fullName}</div>
-                <div>{props.profile.aboutMe}</div>
+                <img src={profile.photos.large ? profile.photos.large : userPhoto} alt="user"/>
+                <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
+                <div>Hi, my name is {profile.fullName}</div>
+                <div>{profile.aboutMe}</div>
                 <div>Contacts:
-                    <div>{props.profile.contacts.facebook}</div>
-                    <div>{props.profile.contacts.website}</div>
-                    <div>{props.profile.contacts.vk}</div>
-                    <div>{props.profile.contacts.twitter}</div>
-                    <div>{props.profile.contacts.instagram}</div>
-                    <div>{props.profile.contacts.youtube}</div>
-                    <div>{props.profile.contacts.github}</div>
-                    <div>{props.profile.contacts.mainLink}</div>
+                    <div>{profile.contacts.facebook}</div>
+                    <div>{profile.contacts.website}</div>
+                    <div>{profile.contacts.vk}</div>
+                    <div>{profile.contacts.twitter}</div>
+                    <div>{profile.contacts.instagram}</div>
+                    <div>{profile.contacts.youtube}</div>
+                    <div>{profile.contacts.github}</div>
+                    <div>{profile.contacts.mainLink}</div>
                 </div>
-                <div>Job preferences: {props.profile.lookingForAJobDescription}</div>
+                <div>Job preferences: {profile.lookingForAJobDescription}</div>
                 <div>
                 </div>
             </div>
--- src/components/common/FormsControls/FormsControls.tsx
+++ src/components/common/FormsControls/FormsControls.tsx
@@ -4,14 +4,14 @@ import {ValidatorType} from '../../../utils/validators/validators';
 import {Field} from 'redux-form';
 
 
-export const FormControl = ({input, meta, FieldType, ...restProps}: any) => {
-    const hasError = meta.touched && meta.error
+export const FormControl = ({input, meta: {touched, error}, FieldType, ...restProps}: any) => {
+    const hasError = touched && error
     return (
         <div className={classes.formControl + ' ' + (hasError ? classes.error : '')}>
             <div>
                 <FieldType {...input} {...restProps}/>
             </div>
-            {hasError && <span>{meta.error}</span>}
+            {hasError && <span>{error}</span>}
         </div>
     );
 };
