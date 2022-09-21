6897419e6e718eb5e472367133fd5175b31c2707
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-21 16:55:58 +0300

    pass MainApp to render instead of App

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import store from "./redux/redux-store";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import {BrowserRouter} from "react-router-dom";
-import {Provider} from "react-redux";
+import MainApp from './App';
 
-ReactDOM.render(
-    <BrowserRouter>
-        <Provider store={store}>
-            <App/>
-        </Provider>
-    </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<MainApp/>, document.getElementById('root'));
