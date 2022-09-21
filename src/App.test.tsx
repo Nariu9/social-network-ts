8cdcebd2a3a66cb7d1ddf0963dd8a1bc72bcabf2
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-21 16:56:16 +0300

    create App test

---
+++ src/App.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import MainApp from './App';
+import ReactDOM from 'react-dom';
+
+test('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<MainApp/>, div);
+  ReactDOM.unmountComponentAtNode(div)
+});
