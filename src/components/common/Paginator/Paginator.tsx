7e860fe06f850bedf87009609415fa21412d2933
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-21 21:54:24 +0300

    refactor Pagination to make it create pages with all users available on the server

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, {useState} from 'react';
 import classes from './Paginator.module.css';
 
 type PaginatorPropsType = {
-    totalUsersCount: number
+    totalItemsCount: number
     pageSize: number
     currentPage: number
     onPageChanged: (page: number) => void
+    portionSize?: number
 }
-export const Paginator: React.FC<PaginatorPropsType> = ({totalUsersCount, pageSize, currentPage, onPageChanged}) => {
+export const Paginator: React.FC<PaginatorPropsType> = ({
+                                                            totalItemsCount,
+                                                            pageSize,
+                                                            currentPage,
+                                                            onPageChanged,
+                                                            portionSize = 10
+                                                        }) => {
 
-    let pagesCount = Math.ceil(totalUsersCount / pageSize)
+    const pagesCount = Math.ceil(totalItemsCount / pageSize)
     let pages = []
     for (let i = 1; i <= pagesCount; i++) {
-        if (pages.length < 10) pages.push(i)
+        pages.push(i)
     }
 
+    const portionCount = Math.ceil(pagesCount / portionSize)
+    const [portionNumber, setPortionNumber] = useState(1)
+    const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1
+    const rightPortionPageNumber = portionNumber * portionSize
+
+
     return <div className={classes.pages}>
-        {pages.map(p => <span key={p}
-                              onClick={() => onPageChanged(p)}
-                              className={currentPage === p ? classes.selectedPage : ''}>{p}</span>)}
+        {portionNumber > 1 && <button onClick={() => setPortionNumber(portionNumber - 1)}>PREV</button>}
+
+        {pages
+            .filter(p => p >= leftPortionPageNumber && p <= rightPortionPageNumber)
+            .map(p => <span key={p}
+                            onClick={() => onPageChanged(p)}
+                            className={currentPage === p ? classes.selectedPage : ''}>{p}</span>)}
+        {portionCount > portionNumber && <button onClick={() => setPortionNumber(portionNumber + 1)}>NEXT</button>}
     </div>
 }
