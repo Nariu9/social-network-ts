ec9d117e49308e746f78bb9bc58dd6a16bf6d572
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-09-22 13:59:28 +0300

    update Preloader position while App is initializing

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ class App extends React.Component<AppPropsType> {
 
     render() {
         if (!this.props.initialized) {
-            return <Preloader/>
+            return <div className={'preloader'}><Preloader/></div>
         }
 
         return (
