52d19863756139ab682960feb6a437559e057ec8
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-07 16:33:23 +0300

    fix auth reducer test

--- src/redux/auth-reducer.test.ts
+++ src/redux/auth-reducer.test.ts
@@ -2,7 +2,7 @@ import {authReducer, AuthStateType, setAuthUserData} from './auth-reducer';
 
 test('user data should be set to state', () => {
 
-    const startState: AuthStateType = {id: null, login: null, email: null, isAuth: false}
+    const startState: AuthStateType = {id: null, login: null, email: null, isAuth: false, captchaUrl: null}
 
     const endState = authReducer(startState, setAuthUserData(1, 'John', 'email@email.com', true))
 
