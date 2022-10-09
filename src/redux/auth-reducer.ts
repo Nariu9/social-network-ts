8307ccedc582e19d3d1d9f945e484598e80910cf
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-10-09 22:46:11 +0300

    add error handling to auth reducer

--- src/redux/auth-reducer.ts
+++ src/redux/auth-reducer.ts
@@ -1,6 +1,9 @@
 import {authAPI, securityAPI} from '../api/api';
 import {AppThunk} from './redux-store';
 import {stopSubmit} from 'redux-form';
+import {openNotificationWithIcon} from '../components/Notification/notifications';
+import {handleServerAppError, handleServerNetworkError} from '../utils/errors/errorHandlers';
+import {AxiosError} from 'axios';
 
 const initialState: AuthStateType = {
     id: null,
@@ -33,10 +36,14 @@ export const setCaptchaUrlAC = (captchaUrl: string) => ({
 
 // thunk creators
 export const getAuthDataThunkCreator = (): AppThunk => async (dispatch) => {
-    const res = await authAPI.getMe()
-    if (res.resultCode === 0) {
-        const {id, login, email} = res.data
-        dispatch(setAuthUserData(id, login, email, true))
+    try {
+        const res = await authAPI.getMe()
+        if (res.resultCode === 0) {
+            const {id, login, email} = res.data
+            dispatch(setAuthUserData(id, login, email, true))
+        }
+    } catch (e) {
+        openNotificationWithIcon('error', 'Error!', 'Please turn on VPN and try again')
     }
 }
 
@@ -44,26 +51,43 @@ export const loginTC = (email: string, password: string, rememberMe: boolean, ca
     authAPI.login(email, password, rememberMe, captcha).then(data => {
         if (data.resultCode === 0) {
             dispatch(getAuthDataThunkCreator())
+            openNotificationWithIcon('success', 'Success!', 'Logged in successfully')
         } else {
             if (data.resultCode === 10) {
                 dispatch(getCaptchaUrlTC())
             }
             const message = data.messages.length > 0 ? data.messages[0] : 'An error has occurred'
             dispatch(stopSubmit('login', {_error: message}))
+            handleServerAppError(data)
         }
+    }).catch(e => {
+        handleServerNetworkError(e)
     });
 }
 
 export const logoutTC = (): AppThunk => async (dispatch) => {
-    const res = await authAPI.logout()
-    if (res.resultCode === 0) {
-        dispatch(setAuthUserData(null, null, null, false, ))
+    try {
+        const res = await authAPI.logout()
+        if (res.resultCode === 0) {
+            dispatch(setAuthUserData(null, null, null, false,))
+            openNotificationWithIcon('success', 'Success!', 'Logged out successfully')
+        } else {
+            handleServerAppError(res)
+        }
+    } catch (e) {
+        const error = e as AxiosError
+        handleServerNetworkError(error)
     }
 }
 
 export const getCaptchaUrlTC = (): AppThunk => async (dispatch) => {
-    const res = await securityAPI.getCaptchaUrl()
-    dispatch(setCaptchaUrlAC(res.url))
+    try {
+        const res = await securityAPI.getCaptchaUrl()
+        dispatch(setCaptchaUrlAC(res.url))
+    } catch (e) {
+        const error = e as AxiosError
+        handleServerNetworkError(error)
+    }
 }
 
 // types
