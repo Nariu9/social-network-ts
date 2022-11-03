9ff49b9a89b356ee130a5a31259d31da380b67ca
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-03 11:34:57 +0200

    update AddMessageForm validators

--- src/components/Dialogs/AddMessageForm/AddMessageForm.tsx
+++ src/components/Dialogs/AddMessageForm/AddMessageForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Field, InjectedFormProps, reduxForm} from 'redux-form';
 import {Textarea} from '../../common/FormsControls/FormsControls';
-import {maxLengthCreator, required} from '../../../utils/validators/validators';
+import {maxLengthCreator} from '../../../utils/validators/validators';
 
 export type FormDataType = {
     newMessageText: string
@@ -13,7 +13,7 @@ const AddMessageForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
     return <form onSubmit={props.handleSubmit}>
         <div>
             <Field placeholder={'Enter your message'} name={'newMessageText'} component={Textarea}
-                   validate={[required, maxLength50]} FieldType={'textarea'}/>
+                   validate={[maxLength50]} FieldType={'textarea'}/>
         </div>
         <div>
             <button>Send</button>
