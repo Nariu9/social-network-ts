743dce4107567ae447d78fc59a391d98822eba10
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-10 22:36:30 +0200

    refactor Login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
-import {InjectedFormProps, reduxForm} from 'redux-form';
-import {createField, Input} from '../common/FormsControls/FormsControls';
-import {required} from '../../utils/validators/validators';
 import {connect} from 'react-redux';
 import {loginTC} from '../../redux/auth-reducer';
 import {RootState} from '../../redux/redux-store';
 import {Redirect} from 'react-router-dom';
-import classes from '../common/FormsControls/FormsControls.module.css';
-import styles from './Login.module.css';
+import classes from './Login.module.scss';
+import LoginForm, {FormDataType} from './LoginForm/LoginForm';
 
 const Login: React.FC<LoginPropsType> = ({loginTC, isAuth, captchaUrl}) => {
     const onSubmit = (formData: FormDataType) => loginTC(formData.email, formData.password, formData.rememberMe, formData.captcha)
@@ -16,59 +13,20 @@ const Login: React.FC<LoginPropsType> = ({loginTC, isAuth, captchaUrl}) => {
         return <Redirect to={'/profile'}/>
     }
 
-    return <div className={styles.container}>
-        <div className={styles.form}>
-            <h1>Login</h1>
-            <LoginReduxForm onSubmit={onSubmit} captchaUrl={captchaUrl}/>
-        </div>
-        <div className={styles.credentials}>
-            <p>To log in get registered
-                <a href={'https://social-network.samuraijs.com/'}
-                   target={'_blank'} rel={'noreferrer'}> here
-                </a>
-            </p>
-            <p>or use common test account credentials:</p>
-            <p>Email: <b>free@samuraijs.com</b></p>
-            <p>Password: <b>free</b></p>
+    return <div className={classes.container}>
+        <div className={classes.login}>
+            <div className={classes.credentials}>
+                <h3 className={classes.title}>Join the network</h3>
+                <p className={classes.subtitle}>A place to share your thoughts and make new connections</p>
+                <div>
+                    <p className={classes.text}>To log in please use common <br/> test account credentials:</p>
+                    <p><b>Email:</b> free@samuraijs.com <br/> <b>Password:</b> free</p>
+                </div>
+            </div>
+            <LoginForm onSubmit={onSubmit} captchaUrl={captchaUrl}/>
         </div>
     </div>
 };
-
-type FormDataType = {
-    email: string
-    password: string
-    rememberMe: boolean
-    captcha: string
-}
-
-type CustomProps = {
-    captchaUrl: string | null
-}
-
-const LoginForm: React.FC<CustomProps & InjectedFormProps<FormDataType, CustomProps>> = ({
-                                                                                             handleSubmit,
-                                                                                             error,
-                                                                                             captchaUrl
-                                                                                         }) => {
-    return <form onSubmit={handleSubmit}>
-        {createField('Email', 'email', [required], Input, 'input')}
-        {createField('Password', 'password', [required], Input, 'input', {type: 'password'})}
-        {createField(null, 'rememberMe', [], Input, 'input', {type: 'checkbox'}, 'remember me')}
-
-        {captchaUrl && <img src={captchaUrl} alt="captcha"/>}
-        {captchaUrl && createField('Symbols on picture', 'captcha', [required], Input, 'input')}
-        {error && <div className={classes.formSummaryError}>
-            {error}
-        </div>}
-        <div>
-            <button>Login</button>
-        </div>
-    </form>
-}
-
-const LoginReduxForm = reduxForm<FormDataType, CustomProps>({
-    form: 'login'
-})(LoginForm)
 
 type LoginPropsType = mapStateToPropsType & mapDispatchToPropsType
 
