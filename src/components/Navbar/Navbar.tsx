14d1e3d176ddfc31541ec15f84c4c328a2c0e8e1
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-11 16:21:55 +0200

    refactor Navbar structure

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,28 @@
-import React from "react";
-import classes from './Navbar.module.css';
-import {NavLink} from "react-router-dom";
+import React from 'react';
+import classes from './Navbar.module.scss';
+import {NavLink} from 'react-router-dom';
+import mainLogo from '../../assets/images/logo-vertical.svg'
+import {BiPaperPlane, BiUser} from 'react-icons/bi';
+import {FiUsers} from 'react-icons/fi';
 
 const Navbar = () => {
     return (
         <nav className={classes.nav}>
-            <div className={classes.item}><NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink></div>
-            <div className={classes.item}><NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink></div>
-            <div className={classes.item}><NavLink to="/users" activeClassName={classes.active}>Users</NavLink></div>
-            <div className={classes.item}><NavLink to="/news" activeClassName={classes.active}>News</NavLink></div>
-            <div className={classes.item}><NavLink to="/music" activeClassName={classes.active}>Music</NavLink></div>
-            <div className={classes.item}><NavLink to="/settings" activeClassName={classes.active}>Settings</NavLink></div>
+            <div className={classes.topBlock}>
+                <div className={classes.logo} style={{backgroundImage: `url(${mainLogo})`}}/>
+                <div className={classes.card}>
+                    <div className={classes.item}>
+                        <NavLink to="/profile" activeClassName={classes.active}><BiUser className={classes.icon}/><span>Profile</span></NavLink>
+                    </div>
+                    <div className={classes.item}>
+                        <NavLink to="/dialogs" activeClassName={classes.active}><span><BiPaperPlane className={classes.icon}/>Messages</span></NavLink>
+                    </div>
+                    <div className={classes.item}>
+                        <NavLink to="/users" activeClassName={classes.active}><FiUsers className={classes.icon}/><span>Users</span></NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className={classes.bottomBlock}/>
         </nav>
     )
 }
