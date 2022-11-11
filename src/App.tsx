346e7e18939665b35f4047acf9c8e8485684a243
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-11 22:34:37 +0200

    remove unused routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import './App.scss';
 import Navbar from './components/Navbar/Navbar';
 import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
-import News from './components/News/News';
-import Music from './components/Music/Music';
-import Settings from './components/Settings/Settings';
 import UsersContainer from './components/Users/UsersContainer';
 import HeaderContainer from './components/Header/HeaderContainer';
 import Login from './components/Login/Login';
@@ -12,7 +9,6 @@ import {connect, Provider} from 'react-redux';
 import {initializeAppTC} from './redux/app-reducer';
 import store, {RootState} from './redux/redux-store';
 import {Preloader} from './components/common/Preloader/Preloader';
-// import 'antd/dist/antd.css'
 
 const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
@@ -42,22 +38,21 @@ class App extends React.Component<AppPropsType> {
         return (
             <>
                 <Switch>
-                    <Route path={'/login'} render={() => <Login/>}/>
-                    <div className="wrapper">
-                        <HeaderContainer/>
-                        <Navbar/>
-                        <div className="content">
-                            <Route path={'/'} exact render={() => <Redirect to={'/profile'}/>}/>
-                            <React.Suspense fallback={<Preloader/>}>
-                                <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
-                                <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
-                                <Route path={'/users'} render={() => <UsersContainer/>}/>
-                            </React.Suspense>
-                            {/* <Route path={'/news'} render={() => <News/>}/>
-                            <Route path={'/music'} render={() => <Music/>}/>
-                            <Route path={'/settings'} render={() => <Settings/>}/>*/}
+                    <>
+                        <Route path={'/login'} render={() => <Login/>}/>
+                        <div className="wrapper">
+                            <HeaderContainer/>
+                            <Navbar/>
+                            <div className="content">
+                                <Route path={'/'} exact render={() => <Redirect to={'/profile'}/>}/>
+                                <React.Suspense fallback={<Preloader/>}>
+                                    <Route path={'/dialogs'} render={() => <DialogsContainer/>}/>
+                                    <Route path={'/profile/:userId?'} render={() => <ProfileContainer/>}/>
+                                    <Route path={'/users'} render={() => <UsersContainer/>}/>
+                                </React.Suspense>
+                            </div>
                         </div>
-                    </div>
+                    </>
                 </Switch>
             </>
         );
