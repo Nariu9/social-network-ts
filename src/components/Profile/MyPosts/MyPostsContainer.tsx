31476e4bf688d6bbc4136f262916ab98bc8d93d7
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-16 18:18:29 +0200

    pass name and deletePost to MyPosts

--- src/components/Profile/MyPosts/MyPostsContainer.tsx
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -1,5 +1,5 @@
 import MyPosts from './MyPosts';
-import {addPostTC, PostType} from '../../../redux/profile-reducer';
+import {addPostTC, deletePostAC, PostType} from '../../../redux/profile-reducer';
 import {connect} from 'react-redux';
 import {RootState} from '../../../redux/redux-store';
 import {AnyAction} from 'redux';
@@ -8,22 +8,26 @@ import {ThunkDispatch} from 'redux-thunk';
 type mapStateToPropsType = {
     posts: Array<PostType>
     avatar: string | undefined
+    name: string | undefined
 }
 type mapDispatchToPropsType = {
     addPost: (newPost: string) => void
+    deletePost: (postId: string) => void
 }
 export type MyPostsPropsType = mapStateToPropsType & mapDispatchToPropsType
 
 const mapStateToProps = (state: RootState): mapStateToPropsType => {
     return {
         posts: state.profilePage.posts,
-        avatar: state.profilePage.profile?.photos?.small
+        avatar: state.profilePage.profile?.photos?.small,
+        name: state.profilePage.profile?.fullName
     }
 }
 
 const mapDispatchToProps = (dispatch: ThunkDispatch<RootState, unknown, AnyAction>): mapDispatchToPropsType => {
     return {
         addPost: (newPost: string) => dispatch(addPostTC(newPost)),
+        deletePost: (postId: string) => dispatch(deletePostAC(postId))
     }
 }
 
