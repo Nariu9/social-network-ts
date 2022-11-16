3b9df02263e71739f6c69c5e4bf5f10575021aa1
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-16 18:12:54 +0200

    add style condition to Preloader component

--- src/components/common/Preloader/Preloader.tsx
+++ src/components/common/Preloader/Preloader.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import preloader from '../../../assets/images/preloader .svg';
+import classes from './Preloader.module.scss';
 
-export const Preloader = () => {
-    return <img src={preloader} alt="preloader"/>
+type PreloaderPropsType = {
+    inside: boolean
+}
+
+export const Preloader = ({inside}: PreloaderPropsType) => {
+    return <img src={preloader} alt="preloader" className={inside ? classes.inside : classes.outside}/>
 };
