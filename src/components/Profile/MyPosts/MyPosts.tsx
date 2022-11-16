2fd31c7c7863151f73bc8206048e57c5083b43b3
Artem Karpukhin <a.karpukhin9@gmail.com>
2022-11-16 18:19:43 +0200

    update MyPosts styles, refactor structure

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,17 +2,16 @@ import classes from './MyPosts.module.scss';
 import React from 'react';
 import Post from './Post/Post';
 import {MyPostsPropsType} from './MyPostsContainer';
-import {Field, InjectedFormProps, reduxForm} from 'redux-form';
-import {maxLengthCreator} from '../../../utils/validators/validators';
-import {Textarea} from '../../common/FormsControls/FormsControls';
-import userPhoto from '../../../assets/images/userPhoto.jpg';
-import {Button} from '../../common/Button/Button';
-
-const maxLength10 = maxLengthCreator(30)
+import PostsForm, {FormDataType} from './PostsForm/PostsForm';
 
 const MyPosts = React.memo((props: MyPostsPropsType) => {
-    let postsElements = props.posts.map(p => <Post key={p.id} id={p.id} message={p.message}
-                                                   likesCount={p.likesCount}/>)
+    let postsElements = props.posts.map(p => <Post key={p.id}
+                                                   id={p.id}
+                                                   message={p.message}
+                                                   likesCount={p.likesCount}
+                                                   name={props.name}
+                                                   avatar={props.avatar}
+                                                   deletePost={props.deletePost}/>)
 
     const addNewPost = (formData: FormDataType) => {
         if (formData.newPostText.trim() === '') return
@@ -23,10 +22,7 @@ const MyPosts = React.memo((props: MyPostsPropsType) => {
         <div className={classes.postsBlock}>
             <div className={classes.postBg}><h3>What's new?</h3>
                 <div className={classes.newPost}>
-                    <img alt="user"
-                         src={props.avatar || userPhoto}
-                         className={classes.avatar}/>
-                    <AddPostReduxForm onSubmit={addNewPost}/>
+                    <PostsForm onSubmit={addNewPost}/>
                 </div>
             </div>
             <div className={classes.posts}>
@@ -37,23 +33,3 @@ const MyPosts = React.memo((props: MyPostsPropsType) => {
 });
 
 export default MyPosts
-
-type FormDataType = {
-    newPostText: string
-}
-
-const PostsForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
-    return <form onSubmit={props.handleSubmit}>
-        <div>
-            <Field name={'newPostText'} placeholder={'write a post'} component={Textarea}
-                   validate={[maxLength10]} FieldType={'textarea'}/>
-        </div>
-        <div>
-            <Button title={'Add post'}/>
-        </div>
-    </form>
-}
-
-const AddPostReduxForm = reduxForm<FormDataType>({
-    form: 'profileAddPostReduxForm'
-})(PostsForm)
